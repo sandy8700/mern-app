6632e4a84a4a89818195fd38eae10e25d0dbd3c2
Santosh kumar <santosh.kumar22@in.ey.com>
2025-11-20 16:36:36 +0530

    Adds backend authentication and DB support; sets up frontend tooling
    Introduces JWT and cookie parsing for user authentication and
    connects the backend to MongoDB. Updates environment configuration
    and includes essential dependencies for authentication, validation,
    and database operations. Sets up React and ESLint dependencies for
    frontend development to enable best practices and modern tooling.

--- uber-app/Backend/app.js
+++ uber-app/Backend/app.js
@@ -4,13 +4,20 @@ dotenv.config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const connectToDb = require('./db/db');
+const cookieParser  = require('cookie-parser')
+
+const userRoutes = require('./routes/user.routes');
 
 app.use(cors());
 app.use(express.json());
-
+connectToDb()
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser())
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('App run sucessfully');
 });
 
+app.use('/users', userRoutes);
 
 module.exports = app;
